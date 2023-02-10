911042bacc4e0034923cf0656e5473fe51a5065e
Mario Salazar <mario10salazar.1995@gmail.com>
2023-02-10 00:58:31 -0500

    Deploy

--- database/connectdb.py
+++ database/connectdb.py
@@ -5,23 +5,22 @@ Created on Thu Nov  3 10:15:31 2022
 @author: Mario
 """
 
-import pyodbc as conn
+import psycopg2 as conn
+from psycopg2 import DatabaseError
 from decouple import config
 from flask_pymongo import PyMongo
 from flask import Flask
 
-# Method to connect SQL Server
-def connect_sqlserver(hostname, dbname, username, password):
+def connect_postgresql(hostname, dbname, username, password):
     try:
-        conexion = conn.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER=' +
-                                hostname + ';DATABASE=' + dbname + ';UID=' + username + ';PWD=' + password)
-        #print("Database connect successfully to SQL Server")
-        #print(config('HOSTNAME'), config('USER_NAME'), config('SERVER'), config('PASSWORD'), config('USERNAME'))
-        return conexion
+        conn_query = "host = '" + hostname + "' dbname = '" + dbname + "' user= '" + username + "' password= '" + password + "'"
+        conn_post = conn.connect(conn_query)
+        print("Database connect successfully to PostgreSQL")
+        return conn_post
     except Exception as e:
         # Atrapar error
-        print("Error to connect to SQL Server: ", e)
-
+        print("Ocurrió un error al conectar a PostgreSQL: ", e)
+        raise Exception(e)
 
 # Method to connect MongoDB Local
 def connect_MongoDB_local():
@@ -47,18 +46,6 @@ def connect_MongoDB(hostname, username, password, database):
     except Exception as e:
         print("Error to connect MongoDB: ", e)
 
-
-def get_connection():
-    try:
-        return connect_sqlserver(
-            config('HOST_NAME'),
-            config('DATABASE'),
-            config('USER_NAME'),
-            config('PASSWORD')
-        )
-    except Exception as ex:
-        raise ex
-
 def get_connectionMongoDB():
     try:
         mongo =  connect_MongoDB(
@@ -70,70 +57,22 @@ def get_connectionMongoDB():
         return mongo
     except Exception as ex:
         raise ex
-
-class Entities:
-    @classmethod
-    def rol_usuarioEntity(self, rol_usuario) -> dict:
-        if rol_usuario:
-            return {
-                "rol_idrol": rol_usuario[0],
-                "rol_nombrerol": rol_usuario[1]
-            }
-        else:
-            return None
-    
-    @classmethod
-    def list_rolUsuarios(self,rol_usuarios) -> list:
-        return [self.rol_usuarioEntity(rol_usuario) for rol_usuario in rol_usuarios]
-
-    @classmethod
-    def personaEntity(self, persona) -> dict:
-        if persona:
-            return {
-            "pers_persona": persona[0],
-            "pers_email": persona[1],
-            "pers_nombres": persona[2],
-            "pers_apellidos": persona[3],
-            "pers_telefono": persona[4],
-            "pers_direccion": persona[5]
-        }
-        else:
-            return None
-
-    @classmethod
-    def listPersonas(self, personas) -> list:
-        return [self.personaEntity(persona) for persona in personas]
-
-    @classmethod
-    def usuarioEntity(self, usuario) -> dict:
-        if usuario:
-            return  {
-			"user_idusuario": usuario[8],
-			"user_password": usuario[11],
-			"user_estado": usuario[9],
-			"user_fecha": usuario[10].strftime('%d/%m/%Y'),
-            "rol_usuario": {
-                "rol_idrol": usuario[0],
-			    "rol_nombrerol": usuario[1]
-            },
-            "persona": {
-                "pers_persona": usuario[2],
-                "pers_email": usuario[3],
-                "pers_nombres": usuario[4],
-                "pers_apellidos": usuario[5],
-                "pers_telefono": usuario[6], 
-                "pers_direccion": usuario[7]
-            }
-        }
-        else:
-            return None
-
-    @classmethod
-    def listUsuarios(self, usuarios) -> list:
-        return [self.usuarioEntity(user) for user in usuarios]
-
-con  = get_connection().cursor()
-row  = con.execute("select p.pers_persona, p.pers_email, p.pers_nombres, p.pers_apellidos, p.pers_telefono, p.pers_direccion from persona p")
-
+# connect_postgresql(hostname, dbname, username, password):
+def get_connection():
+    try:
+        connection = connect_postgresql(
+            config('HOST_NAME_HEROKU'),
+            config('DATABASE_HEROKU'),
+            config('USER_NAME_HEROKU'),
+            config('PASSWORD_HEROKU')
+        )
+        return connection
+    except Exception as ex:
+        raise ex
+a = get_connection()
+a = a.cursor()
+a.execute("select * from user_usuario")
+row = a.fetchall()
 for p in row:
     print(p)
+
